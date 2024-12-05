89c9e3f01555f1486116e002fb06aceaf52dd0f4
Andrey Oliveira <andreyoliveira222@gmail.com>
2024-12-05 17:05:06 -0300

    aula 1

---
+++ aula1.py
@@ -0,0 +1,10 @@
+# Docstring não é comentário. O Python, lê essa linha de código, porém, os desenvolvedores em alguns casos, costumam, utilizar como comentários.
+
+"""
+    Aqui é usado em algumas situações, é utilizado, para comentar mais de uma linha, mas não é um comentário.
+"""
+
+# Permite escrever um comentário
+print(123) # Na frente
+# Abaixo
+print(123)

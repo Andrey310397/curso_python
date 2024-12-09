9feb83375c99c9ad9f4153e0788e460b378c23e4
Andrey Oliveira <andreyoliveira222@gmail.com>
2024-12-09 18:16:56 -0300

    Tipos primitivos de dados (str, int, float e bool)

---
+++ aula3.py
@@ -0,0 +1,23 @@
+"""
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+
+# Aspas simples
+print('Luiz Otávio') # Ele saberá que é uma string
+
+# Aspas duplas
+print("Luiz Otávio")
+
+# Escape
+print("Luiz \"Otávio\"") # a \ está avisando que o caractere não precisa ser interpretado pelo interpretador
+
+# r
+print(r"Luiz \"Otávio") # o caractere r, faz com que a \, seja exibida na tela no momento da execução. Isso é mais utilizado para expressões regulares.
+
+# Particularmente prefiro escrever a string com aspas simples, onde pode ser utilizado as aspas duplas sem ser interpretado ou vice-versa
+print("Luiz 'Otávio'")
+print('Luiz "Otávio"')

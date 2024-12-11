c73c244956ff056f598365d02b156ff664b8aaa1
Andrey Oliveira <andreyoliveira222@gmail.com>
2024-12-11 18:26:06 -0300

    exercicio de variaveis

---
+++ aula8.py
@@ -0,0 +1,16 @@
+# Exercício de variáveis
+# Pegar as variáveis que estão no print e criar de acordo com seus valores e respectivas respotas
+
+nome = 'Andrey'
+sobrenome = 'Pereira'
+idade = 27
+ano_nascimento = 2024 - idade
+maior_de_idade = idade >= 18
+altura_metros = 1.81
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('É maior de idade:', maior_de_idade)
+print('Altura em metros:', altura_metros)

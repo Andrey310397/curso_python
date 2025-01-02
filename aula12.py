5332a3c35365172b07a1b542e26cc61fd0494fcb
Andrey Oliveira <andreyoliveira222@gmail.com>
2025-01-02 17:23:40 -0300

    Operadores aritméticos

---
+++ aula12.py
@@ -0,0 +1,9 @@
+# Luiz Otávio tem 1.80 de altura,
+# pesa 95 quilos e seu IMC É ....
+
+nome = 'Andrey'
+altura = 1.81
+peso = 110
+imc = peso / (altura ** 2)
+
+print(f'Andrey tem {altura}, pesa {peso} e seu IMC é {imc}')
